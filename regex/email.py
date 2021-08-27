6509b86b17389bdff2279f0dc8ddb2f1efa3723d
Przemysław Samsel (SecuRing) <psamsel@securing.pl>
2021-08-27 16:38:20 +0200

    text + regex

--- regex/email.py
+++ regex/email.py
@@ -3,9 +3,8 @@ import re
 nr_of_lines = int(input())
 found = []
 
-Regex_Pattern = r"\b[a-zA-Z0-9_]+\b@\b\w+\b.\b\w+\b"
 for x in [input() for x in range(nr_of_lines)]:
-    found += re.findall(Regex_Pattern, x)
+    found += re.findall(r"\b[a-zA-Z0-9_]+\b@\b\w+\b.\b\w+\b", x)
     
 print(';'.join(sorted(list(set(found)))))
 
